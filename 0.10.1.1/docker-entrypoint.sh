d1d44ec265d81e05d7764c489f80dc6372bcd924
gprivitera <gianluca.privitera@hotmail.it>
2017-02-06 16:30:49 -0600

    First commit

---
+++ 0.10.1.1/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Generate the server.properties only if it doesn't exist
+if [ ! -f "$KAFKA_CONF_DIR/server.properties" ]; then
+    CONFIG="$KAFKA_CONF_DIR/server.properties"
+
+    echo "broker.id=$KAFKA_BROKER_ID" >> "$CONFIG"
+    echo "zookeeper.connect=$KAFKA_ZOOKEEPER_CONNECT" >> "$CONFIG"
+    echo "log.dirs=$KAFKA_LOGS_DIR" >> "$CONFIG"
+    echo "num.partitions=$KAFKA_NUM_PARTITIONS" >> "$CONFIG"
+    echo "default.replication.factor=$KAFKA_DEFAULT_REPLICATION_FACTOR" >> "$CONFIG"
+fi
+
+exec "$@"
