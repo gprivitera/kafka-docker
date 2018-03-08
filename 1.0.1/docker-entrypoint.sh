4392a05484c21fb74d7cbf18099ec73b6f87a208
gprivitera <gpriviter4@gmail.com>
2018-03-08 10:05:14 -0600

    bump to 1.0.1

--- 1.0.1/docker-entrypoint.sh
+++ 1.0.1/docker-entrypoint.sh
@@ -12,7 +12,15 @@ if [ ! -f "$KAFKA_CONF_DIR/server.properties" ]; then
     echo "num.partitions=$KAFKA_NUM_PARTITIONS" >> "$CONFIG"
     echo "default.replication.factor=$KAFKA_DEFAULT_REPLICATION_FACTOR" >> "$CONFIG"
     echo "offsets.topic.replication.factor=$KAFKA_OFFSET_TOPIC_REPLICATION_FACTOR" >> "$CONFIG"
-
+    
+    if [ ! -z "$CURRENT_KAFKA_VERSION" ]; then
+      echo "inter.broker.protocol.version=$CURRENT_KAFKA_VERSION" >> "$CONFIG"
+    fi 
+    
+    if [ ! -z "$CURRENT_MESSAGE_FORMAT_VERSION" ]; then
+      echo "log.message.format.version=$CURRENT_MESSAGE_FORMAT_VERSION" >> "$CONFIG"
+    fi 
+    
     if [ ! -z "$KAFKA_LISTENERS" ]; then
         echo "listers=$KAFKA_LISTENERS" >> "$CONFIG"
     fi
